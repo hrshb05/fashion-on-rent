9a77479000c48443d74ffabbc0dbc4756cb18078
Harshit Bhushan <55655045+hrshb05@users.noreply.github.com>
2022-07-21 11:45:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import Signup from "./Components/Login/Signup";
 import Pag from "./Pages/Homepage";
 import Ad from "./Pages/ad";
 import Form from "./Pages/category";
-import Header from "./Components/Header"
-import Footer from "./Components/footer";
 import View from "./Pages/viewitem";
 import Logtoview from "./Pages/logtoview";
 import Mens from "./Pages/Mens";
 import Women from "./Pages/Womens";
 import Jewellery from "./Pages/Jewellery";
 import Kids from "./Pages/Kids";
-import {Route , Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
