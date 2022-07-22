f967a84b4eb1d06958df45976b83ab6c2dfadfe9
Harshit Bhushan <harshitbhushan5@gmail.com>
2022-07-22 12:05:16 +0530

    Update images.js

--- src/Pages/images.js
+++ src/Pages/images.js
@@ -3,11 +3,11 @@ const i2  = "view1.png";
 const i3 = "view2.png";
 const i4 = "view3.png";
 
-const images = {
+const images = [
     i1,
     i2,
     i3,
     i4
-};
+];
 
 export default images;
